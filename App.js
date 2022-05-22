57f966a68d9da89929774786a75f7c5d912f2c94
Shamim Ullah <shamim.ullah.86@gmail.com>
2022-05-22 18:07:32 +0100

    colors component and fonts imported

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_700Bold,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold
+} from '@expo-google-fonts/montserrat';
+
+import Colors from './assets/colors/colors';
+
 
 export default function App() {
+  
+  let [fontsLoaded] = useFonts({
+      Montserrat_400Regular,
+      Montserrat_700Bold,
+      Montserrat_500Medium,
+      Montserrat_600SemiBold
+    });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.font}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +39,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  font: {
+    fontFamily: "Montserrat_400Regular",
+    color: Colors.textDark,
+  }
 });

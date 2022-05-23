9f474807356c149f427a454e28b416d25592c39e
Shamim Ullah <shamim.ullah.86@gmail.com>
2022-05-23 08:20:56 +0100

    Added to GitHub

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Montserrat_500Medium,
   Montserrat_600SemiBold
 } from '@expo-google-fonts/montserrat';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import Colors from './assets/colors/colors';
 
@@ -24,24 +25,6 @@ export default function App() {
     return null;
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.font}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return <Text>Open up App.js to start working on your app!</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  font: {
-    fontFamily: "Montserrat_400Regular",
-    color: Colors.textDark,
-  }
-});
+};
